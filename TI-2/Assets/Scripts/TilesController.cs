7ac586e6b7581cca09a0796475f3cd2fb8d9a0c9
MaryLife <112979364+MaryLife@users.noreply.github.com>
2022-09-29 11:20:31 -0300

    MUDANÇAS!!

--- TI-2/Assets/Scripts/TilesController.cs
+++ TI-2/Assets/Scripts/TilesController.cs
@@ -4,19 +4,23 @@ using UnityEngine;
 
 public class TilesController : MonoBehaviour
 {
-    public Transform startPoint;
-    public Transform endPoint;
-    public GameObject[] obstacles;
+    public Transform startPoint; // GameObject marcando o início do tile
+    public Transform endPoint; // GameObject marcando o final do tile
+    public GameObject[] obstacles; // Lista de obstáculos
 
     public void ActivateRandomObstacle()
     {
-        DeactivateAllObstacles();
-        int randomNumber = Random.Range(0, obstacles.Length); //no dele não possui o +1
+        DeactivateAllObstacles(); // Desliga todos os obstáculos antes de ligar um aleatoriamente
+
+        int randomNumber = Random.Range(0, obstacles.Length);
         obstacles[randomNumber].SetActive(true);
     }
-    public void DeactivateAllObstacles()
+
+    public void DeactivateAllObstacles() // Desliga todos os obstáculos
     {
         for (int i = 0; i < obstacles.Length; i++)
+        {
             obstacles[i].SetActive(false);
+        }
     }
 }
--- TI-2/Assets/Scripts/TilesGenerator.cs
+++ TI-2/Assets/Scripts/TilesGenerator.cs
@@ -5,50 +5,56 @@ using UnityEngine;
 public class TilesGenerator : MonoBehaviour
 {
     public Camera mainCamera;
-    public Transform startPoint;
-    public TilesController tilePrefab;
-    public float moveSpeed = 8;
-    public int tilesPreSpawn = 5;
-    public int tilesNoObstacle = 3; //Sem obstaculos no início do jogo
+    public Transform startPoint; // Onde o jogo deve começar
+    public TilesController tilesPrefab;
+    public float moveSpeed = 12;
+    public int tilesPreSpawn = 5; // Quantidade de tiles que eu quero ter sempre em cena
+    public int tilesNoObstacle = 3; // Quantidade de tiles sem obstáculos no início do jogo
 
-    List<TilesController> spawnedTiles = new List<TilesController>();
+    List<TilesController> spawnedTiles = new List<TilesController>(); // Lista de tiles
+
     public static TilesGenerator Instance;
 
     private void Start()
     {
         Instance = this;
-
         Vector3 spawnPos = startPoint.position;
         int tilesNoObstacleTemp = tilesNoObstacle;
 
-        for (int i = 0; i < tilesPreSpawn; i++) 
+        for (int i = 0; i < tilesPreSpawn; i++)
         {
-            spawnPos -= tilePrefab.startPoint.position;
-            TilesController currentTile = Instantiate(tilePrefab, spawnPos, Quaternion.identity);
+            spawnPos -= tilesPrefab.startPoint.position; // Calcula a diferença do offset do tile
+            TilesController currentTile = Instantiate(tilesPrefab, spawnPos, Quaternion.identity);
             if (tilesNoObstacle > 0)
             {
                 currentTile.DeactivateAllObstacles();
                 tilesNoObstacleTemp--;
-            }else
+            }
+            else
+            {
                 currentTile.ActivateRandomObstacle();
-
-            spawnPos = currentTile.endPoint.position;
-            currentTile.transform.SetParent(transform);
-            spawnedTiles.Add(currentTile);
+            }
+            spawnPos = currentTile.endPoint.position; // Posição para criar o próximo tile
+            currentTile.transform.SetParent(transform); // Tiles viram filhos do objeto
+            spawnedTiles.Add(currentTile); // Adiciona o tile na lista
         }
     }
 
     private void Update()
     {
-        transform.Translate(Time.deltaTime * moveSpeed * (- spawnedTiles[0].transform.forward)); //apenas mudei de ordem, mas o problema continua
+        transform.Translate(-spawnedTiles[0].transform.forward * Time.deltaTime * moveSpeed); // Move este objeto para frente
 
-        if (mainCamera.WorldToViewportPoint(spawnedTiles[0].endPoint.position).x < -0.3f) //no dele ele colocou negativo ao inves -erros ;-;- de 0
+        // Esse if verifica se o objeto 0 da lista (o primeiro) passou da visão da câmera no eixo X negativo,
+        // ou seja, se ele está à esquerda da câmera, fora da área de renderização
+        if (mainCamera.WorldToViewportPoint(spawnedTiles[0].endPoint.position).z < -0.3f)
         {
+            // Move o tile pra frente se ele tiver saído da câmera
             TilesController tileTemp = spawnedTiles[0];
-            spawnedTiles.RemoveAt(0);
+            spawnedTiles.RemoveAt(0); // Remove o tile da lista
+            // Reposiciona o tile fora da visão da câmera e o coloca no final
             tileTemp.transform.position = spawnedTiles[spawnedTiles.Count - 1].endPoint.position - tileTemp.startPoint.localPosition;
-            tileTemp.ActivateRandomObstacle();
-            spawnedTiles.Add(tileTemp);
+            tileTemp.ActivateRandomObstacle(); // Ativa um obstáculo aleatório
+            spawnedTiles.Add(tileTemp); // Readiciona o tile à lista (no final)
         }
     }
 
